3de04de82781b69bb4e8bdd84f27477ef68a4e2b
Emily <emily@sbl-emily-usv1.(none)>
2015-05-27 15:08:55 +0100

    Nav message read.

--- asv_pilot/src/asv_pilot.py
+++ asv_pilot/src/asv_pilot.py
@@ -6,6 +6,7 @@ roslib.load_manifest('asv_pilot')
 
 import rospy
 import numpy as np
+np.set_printoptions(precision=3, suppress=True)
 from transformations import euler_from_quaternion
 
 import controllers as ctrl
@@ -20,7 +21,7 @@ from nav_msgs.msg import Odometry
 TOPIC_WAYPOINT = '/path/waypoint'
 TOPIC_THROTTLE = '/motors/throttle'
 ODOMETRY_NAV = '/nav/odometry'
-ODOMETRY_TIMEOUT = 0.5 # seconds
+ODOMETRY_TIMEOUT = 5 # seconds
 LOOP_RATE = 10  # Hz
 
 
@@ -29,7 +30,7 @@ class Pilot(object):
         self.name = name
 
         # latest throttle received
-        self.pos = np.zeros(6)
+        self.pos = np.zeros(6)  # [x, y, z, roll, pitch, yaw]
         self.des_pos = np.zeros(6)
 
         self.last_odometry_t = 0
@@ -46,27 +47,30 @@ class Pilot(object):
 
     def loop(self):
         # if message is old and throttle is non-zero then set to zero
-        if (rospy.Time.now().to_sec() - self.last_odometry_t) > ODOMETRY_TIMEOUT:
+        if (rospy.Time.now().to_sec() - self.last_odometry_t) > ODOMETRY_TIMEOUT and self.odometry_switch:
             self.odometry_switch = False
             rospy.logerr('Odometry outdated')
 
+        rospy.loginfo('POS: %s DES_POS: %s', self.pos, self.des_pos )
+
         if self.odometry_switch is True:
-            throttle = ctrl.compute_throttle(self.pos, self.des_pos)
+            throttle = np.zeros(6)
+            # throttle = ctrl.compute_throttle(self.pos, self.des_pos)
             throttle_msg = ThrusterCommand()
             throttle_msg.header.stamp = rospy.Time().now()
             throttle_msg.throttle = throttle
 
     def handle_odometry(self, msg):
         try:
-            print 'position', msg.pose.pose.position
-            self.pos[0:3] = np.array(msg.pose.pose.position)
-            rotation = euler_from_quaternion(np.array(msg.pose.pose.orientation))
-            print 'orientation', rotation
-            self.pos[3:6] = rotation
+            pos = msg.pose.pose.position
+            self.pos[0:3] = np.array([pos.x, pos.y, pos.z])
+            rot = msg.pose.pose.orientation
+            quaternion = np.array([rot.w, rot.x, rot.y, rot.z])
+            self.pos[3:6] = euler_from_quaternion(quaternion)
             self.last_odometry_t = msg.header.stamp.to_sec()
             self.odometry_switch = True
         except Exception as e:
-            rospy.logerr('%s Bad odometry message format, skipping!', e)
+            rospy.logerr('Bad odometry message format, skipping!')
 
     def handle_waypoint(self, msg):
         try:
@@ -89,6 +93,6 @@ if __name__ == '__main__':
         except rospy.ROSInterruptException:
             rospy.loginfo('%s caught ros interrupt!', name)
         except Exception as e:
-            rospy.logfatal('%s caught exception and dying!', name)
+            rospy.logfatal('%s caught exception and dying!', e)
 
 
