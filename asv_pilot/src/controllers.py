1533ed4434d666bb8145e253d830ddf4afdc6916
Marian <M.Andrecki@hw.ac.uk>
2015-05-27 16:07:06 +0100

    Point and shoot controller should be now functional

--- asv_pilot/src/controllers.py
+++ asv_pilot/src/controllers.py
@@ -4,17 +4,28 @@
 import numpy as np
 np.set_printoptions(precision=3, suppress=True)
 
-RUDDER_POS_X = 0.4  # distance from the centre of mass
-# Rudder is at angle 0 for throttle 0, and at position 0.4 for throttle 100.
-RUDDER_ANGLE_MAX = 0.4  # in radians (23 degrees).
-MAX_LINEAR_FORCE = 100  # for every linear component (not in Newtons)
-MAX_TORQUE = MAX_LINEAR_FORCE * RUDDER_POS_X * np.sin(RUDDER_ANGLE_MAX)
+
+MAX_THRUST = 100
+MAX_RUDDER = 100
+MAX_RUDDER_ANGLE = np.pi/2  # error in yaw at which maximum rudder throttle is applied
+MAX_THRUST_DISTANCE = 50  # distance above which the thrust is maximum
+TURNING_THR_ANGLE = np.pi/4  # error below which vehicle shoots towards the goal
+SLOWDOWN_OFFSET = 10  # distance at which the vehicle stops the thruster
+TURNING_THRUST = 40
+
+# RUDDER_POS_X = 0.4  # distance from the centre of mass
+# # Rudder is at angle 0 for throttle 0, and at position 0.4 for throttle 100.
+# RUDDER_ANGLE_MAX = 0.4  # in radians (23 degrees).
+# MAX_LINEAR_FORCE = 100  # for every linear component (not in Newtons)
+# MAX_TORQUE = MAX_LINEAR_FORCE * RUDDER_POS_X * np.sin(RUDDER_ANGLE_MAX)
 
 def wrap_pi(angle):
     return ((angle + np.pi) % (2*np.pi)) - np.pi
 
 def point_shoot(pose, des_pos):
-    """
+    """Simplist controller which first adjusts orientation (P control) and then moves forward while maintaining
+    the right orientation. The vehicle has to stop the thruster before reaching the goal as there is no way to
+    brake.
 
     :param pose: current pose [x, y, z, roll, pitch, yaw]
     :param des_pos: goal pose [x, y, z, roll, pitch, yaw]
@@ -32,15 +43,21 @@ def point_shoot(pose, des_pos):
     error_xy = des_pos[0:2] - pose[0:2]
     angle_to_goal = np.arctan2(error_xy[1], error_xy[2])
 
+    # compute rudder throttle
     error_yaw = wrap_pi(angle_to_goal - pose[5])
 
+    # map the error to the throttle, note that throttle is max for error greater than MAX_RUDDER_ANGLE radians
+    rudder_throttle = MAX_RUDDER * error_yaw * (1 / MAX_RUDDER_ANGLE)
+    rudder_throttle = max(-100, min(100, rudder_throttle))
 
-    rudder_throttle = f_yaw/MAX_TORQUE * 100
+    # compute thruster throttle
+    # if the vehicle is pointed in the right direction
+    if abs(error_yaw) < TURNING_THR_ANGLE:
+        distance = np.linalg.norm(error_xy)
+        thrust_throttle = MAX_THRUST * (distance - SLOWDOWN_OFFSET) * (1 / MAX_THRUST_DISTANCE)
+    else:
+        thrust_throttle = TURNING_THRUST
 
-    rudder_angle = RUDDER_ANGLE_MAX * rudder_throttle / 100
-    xy_angle = np.atan2(force[1], force[0])
-
-    # how similar is the direction of the force applied to the desired linear force? normalised to 1
-    thrust_coef = np.abs(wrap_pi(xy_angle - rudder_angle))/(2*np.pi)
-
+    throttle[0] = thrust_throttle
+    throttle[1] = rudder_throttle
     return throttle
