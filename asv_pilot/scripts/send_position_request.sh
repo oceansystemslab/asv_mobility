23028b9fe2c7874f654ce8a6983554c9b0838381
Marian <M.Andrecki@hw.ac.uk>
2015-06-08 15:33:15 +0100

    Split scripts into scripts and paths

--- asv_pilot/scripts/send_position_request.sh
+++ asv_pilot/scripts/send_position_request.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-rostopic pub /pilot/position_req vehicle_interface/PilotRequest "header:
-  seq: 0
-  stamp: {secs: 0, nsecs: 0}
-  frame_id: ''
-priority: 0
-position: [-20.0, -20.0, 0.0, 0.0, 0.0, 0.0]
-velocity: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-limit_velocity: [0, 0, 0, 0, 0, 0]
-disable_axis: [0, 0, 0, 0, 0, 0]" 
+rostopic pub /pilot/position_req vehicle_interface/PilotRequest "position: [-20.0, -20.0, 0.0, 0.0, 0.0, 0.0]"
