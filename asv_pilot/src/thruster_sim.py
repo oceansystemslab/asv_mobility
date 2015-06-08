305b9410d0fa530f3b1b45bfe27fcf4edd7ce4ce
Marian <M.Andrecki@hw.ac.uk>
2015-06-08 14:52:30 +0100

    Added licence note

--- asv_pilot/src/thruster_sim.py
+++ asv_pilot/src/thruster_sim.py
@@ -1,12 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Software License Agreement (BSD License)
+#
+#  Copyright (c) 2014, Ocean Systems Laboratory, Heriot-Watt University, UK.
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the following
+#     disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+#   * Neither the name of the Heriot-Watt University nor the names of
+#     its contributors may be used to endorse or promote products
+#     derived from this software without specific prior written
+#     permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+#  Original authors:
+#   Valerio De Carolis, Marian Andrecki, Corina Barbalata, Gordon Frost
+
 import roslib
 roslib.load_manifest('asv_pilot')
 
 import rospy
 import numpy as np
-import sys
+# import sys
 
 import emily_physics as ep
 
@@ -90,11 +126,11 @@ if __name__ == '__main__':
         except rospy.ROSInterruptException:
             ts.set_force_neutral()
             rospy.loginfo('%s caught ros interrupt!', name)
-        except Exception as e:
-            ts.set_force_neutral()
-            rospy.logfatal('%s', e)
-            rospy.logfatal('%s caught exception and dying!', name)
-            sys.exit(-1)
+        # except Exception as e:
+        #     ts.set_force_neutral()
+        #     rospy.logfatal('%s', e)
+        #     rospy.logfatal('%s caught exception and dying!', name)
+        #     sys.exit(-1)
 
 
 
