625cccc732d8eadacb21c1505a646e9f030624e1
Marian <marian.andrecki@gmail.com>
2015-08-09 10:58:15 +0100

    Added scripts for extraction of topics from messages and processing data in notebook.

---
+++ asv_pilot/bags/data_extraction.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# script for data extraction
+DIR=$1
+PWD=$(pwd)
+
+# topics names
+POSITION_REQ="/emily/pilot/position_req"
+
+### script below ###
+echo $DIR
+
+if [[ ! -n $DIR ]]
+then
+	echo "Please provide a directory"
+	exit 0
+fi
+
+# change dir
+cd $DIR
+
+for BAG in $(ls | grep .bag)
+do
+  echo "Processing $BAG ..."
+  PREFIX=$(echo $BAG | cut -d '.' -f 1)
+  
+	rostopic echo -p -b $BAG /emily/nav/nav_sts > "${PREFIX}_nav_sts.csv"
+	rostopic echo -p -b $BAG $POSITION_REQ > "${PREFIX}_position_req.csv"
+
+  
+done
+
+exit 0
